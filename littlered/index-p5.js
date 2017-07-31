5aabb3fe9c76e8ef132139af082fbb7327aed794
Nate Graves <natesgraves@gmail.com>
2017-07-31 17:47:21 -0400

    Make it yellow

--- littlered/index-p5.js
+++ littlered/index-p5.js
@@ -16,13 +16,11 @@ function Fog()  {
 
 Fog.prototype.add = function(x, y) {
   this.dots.push(new Dot(x, y));
-  console.log(this.dots.length);
 }
 
 Fog.prototype.update = function() {
   clear();
   this.add(mouseX, mouseY);
-  console.log(this.dots.length);
 
   for (var i = 0; i < this.dots.length; i++) {
     if (this.dots[i].lifespan > 0) {
@@ -41,13 +39,12 @@ function Dot(x, y) {
 
 Dot.prototype.update = function() {
   this.lifespan -= 6.0;
-  //console.log(this.lifespan);
 }
 
 Dot.prototype.display = function() {
   if (this.lifespan >  2) {
-    stroke(240, this.lifespan);
-    fill(240, this.lifespan);
+    stroke(235, 255, 0, this.lifespan);
+    fill(235, 255, 0, this.lifespan);
     ellipse(this.position.x, this.position.y, 150, 150);
   }
 }
