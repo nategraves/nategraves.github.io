50baa68c9fd24d31f8ea903f6e0011c280a9f7d5
Nate Graves <natesgraves@gmail.com>
2017-07-31 13:07:18 -0400

    Better

---
+++ littlered/index-p5.js
@@ -0,0 +1,12 @@
+function setup() {
+  var canvas = createCanvas(windowWidth, 500);
+  canvas.parent('interactive');
+}
+
+function draw() {
+  if (mouseIsPressed) {
+    fill(255);
+    stroke(255);
+    ellipse(mouseX, mouseY, 80, 80);
+  }
+}
