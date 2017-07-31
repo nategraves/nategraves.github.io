5487d3a09b01c73506a4bd8ce61941ec8ed8470c
Nate Graves <natesgraves@gmail.com>
2017-07-31 18:48:17 -0400

    More interactive

--- littlered/index-p5.js
+++ littlered/index-p5.js
@@ -6,21 +6,32 @@ function setup() {
   fog = new Fog();
 }
 
+function mousePressed() {
+  fog.addMovingDots(mouseX, mouseY);
+}
+
 function draw() {
   fog.update();
 }
 
 function Fog()  {
   this.dots = [];
+  this.movingDots = [];
 }
 
-Fog.prototype.add = function(x, y) {
+Fog.prototype.addDot = function(x, y) {
   this.dots.push(new Dot(x, y));
+}
+
+Fog.prototype.addMovingDots = function(x, y) {
+  for (var i = 0; i < 100; i++) {
+    this.movingDots.push(new MovingDot(createVector(mouseX, mouseY)));
+  }
 }
 
 Fog.prototype.update = function() {
   clear();
-  this.add(mouseX, mouseY);
+  this.addDot(mouseX, mouseY);
 
   for (var i = 0; i < this.dots.length; i++) {
     if (this.dots[i].lifespan > 0) {
@@ -28,6 +39,15 @@ Fog.prototype.update = function() {
       this.dots[i].display();
     } else {
       this.dots.splice(i, 1);
+    }
+  }
+
+  for (var i = 0; i < this.movingDots.length; i++) {
+    if (this.movingDots[i].lifespan > 0) {
+      this.movingDots[i].update();
+      this.movingDots[i].display();
+    } else {
+      this.movingDots.splice(i, 1);
     }
   }
 }
@@ -46,5 +66,30 @@ Dot.prototype.display = function() {
     stroke(235, 255, 0, this.lifespan);
     fill(235, 255, 0, this.lifespan);
     ellipse(this.position.x, this.position.y, 150, 150);
+  }
+}
+
+function MovingDot(position) {
+  this.velocity = createVector(random(-5, 5), random(-5, 5));
+  this.position = position.copy();
+  this.lifespan = 240.0;
+  this.radius = random(20, 150);
+}
+
+MovingDot.prototype.run = function() {
+  this.update();
+  this.display();
+}
+
+MovingDot.prototype.update = function() {
+  this.position.add(this.velocity);
+  this.lifespan -= 2;
+}
+
+MovingDot.prototype.display = function() {
+  if (this.lifespan >  2) {
+    stroke(235, 255, 0, this.lifespan);
+    fill(235, 255, 0, this.lifespan);
+    ellipse(this.position.x, this.position.y, this.radius, this.radius);
   }
 }
