33a8473c97a8777eb34dd6cab6b53874d3361004
Nate Graves <natesgraves@gmail.com>
2017-07-31 11:18:13 -0400

    Looking a bit closer to mock

--- littlered/index.js
+++ littlered/index.js
@@ -1,78 +1,64 @@
-    // The amount of circles we want to make:
-    var count = 150;
-    var logoSVG = document.getElementById('logo');
-    var logoItem = null;
-    var vb = view.bounds;
+document.onload = start();
 
-    // Create a symbol, which we will use to place instances of later:
-    var path = new Path.Circle({
-      center: [0, 0],
-      radius: 10,
-      fillColor: 'white'
-    });
+var start = function() {
+  var canvas = document.getElementById('first');
+  paper.setup(canvas);
+  var logoSVG = document.getElementById('logo');
+  var logoItem = null;
+  var svgSymbol = null;
+  var vb = view.bounds;
 
-    var mainPath2 = new Path.Circle({
-      center: vb.center,
-      radius: vb.height / 4,
-      fillColor: '#fd5139',
-      name: 'main'
-    });
+  var path = new paper.Path.Circle({
+    center: [-10, -10],
+    radius: 10,
+    fillColor: 'white'
+  });
 
-    var mainPath2 = new Path.Circle({
-      center: vb.center,
-      radius: vb.height / 4,
-      fillColor: 'white',
-      opacity: 0.4,
-      name: 'main2'
-    });
-
-    project.importSVG(logoSVG, function(logoItem) {
-      logoItem.bringToFront();
-      logoItem.name = 'logo';
-      var delta = logoItem.bounds.center - vb.center;
-      logoItem.translate(delta);
-      /*
-      var result = mainPath.subtract(logoItem[1]);
-      result.fillColor = 'black';
-      result.bringToFront();
-      result.position = [200,200];
-      result.name = 'logo';
-      */
-    });
-
-    var symbol = new Symbol(path);
-
-    for (var i = 0; i < count; i++) {
-      var center = Point.random() * view.size;
-      var placedSymbol = symbol.place(center);
-      placedSymbol.scale(i / count);
+  var svgPaths = [];
+  project.importSVG(logoSVG, function(svg) {
+    logoItem = svg;
+    //svgSymbol = new Symbol(svg);
+    var delta =  vb.center - logoItem.bounds.center;
+    logoItem.translate(delta);
+    logoItem.onMouseEnter = function(event) {
+      var topLeft = clone
+      this.remove();
+    };
+    for (var i = 1; i < logoItem.children.length; i++) {
+      var child = logoItem.children[i];
+      if (child.children && child.children.length > 0) {
+        for (var j = 0; j < child.children.length; j++) {
+          if (child.children[j].segments && child.children.length > 0) {
+            svgPaths.push(child.children[j].segments);
+          }
+        }
+      } else {
+        if (child.segments) {
+          svgPaths.push(child.segments)
+        }
+      }
     }
+    logoItem.bringToFront();
+    logoItem.name = 'logo';
+  });
 
-    function onFrame(event) {
-      for (var i = 0; i < count; i++) {
-        var item = project.activeLayer.children[i];
-        if (item.name === 'main' || item.name === 'main2') { 
-          if (event.count % 3 === 0 && item.name == 'main2') {
-            item.radius += math.randomInt(-50, 50);
-            item.position.x += math.randomInt(-10, 10);
-            item.position.y += math.randomInt(-10, 10);
-            item.sendToBack();
-          } else {
-            item.position = view.bounds.center;
-            item.radius = view.bounds.height / 2;
-          }
-        } else if (item.name === 'result' || item.name == 'logo') {
-          item.position = view.bounds.center;
-          var segments = item.children[1].children[0].children[0].segments;
-          for (var i = 0; i < segments.length; i++) {
-            var segment = segments[i];
-            var sinus = Math.sin(event.time * 3 + i);
-          }
-        } else {
-          item.position.x += item.bounds.width / 20;
-          if (item.bounds.left > view.size.width) {
-            item.position.x = -item.bounds.width;
+  function onFrame(event) {
+    if (logoItem) {
+      var delta =  vb.center - logoItem.bounds.center;
+      logoItem.translate(delta);
+    }
+    if (svgPaths.length) {
+      for (var i = 0; i <= svgPaths.length; i++) {
+        var path = svgPaths[i];
+        if (path && path.length > 0) {
+          for (var p = 0; p < path.length; p++) {
+            var sinus = Math.sin(event.time * 3 + p);
+            var point = path[p].point;
+            // console.log(math.round(sinus * 5));
+            // point.y = sinus * 1;
           }
         }
       }
-}
+    }
+  }
+};
