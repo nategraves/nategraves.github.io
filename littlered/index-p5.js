0f215f18518579ede3249df51ad9a675f64c11df
Nate Graves <natesgraves@gmail.com>
2017-07-31 14:47:49 -0400

    Ohhh, there we go

--- littlered/index-p5.js
+++ littlered/index-p5.js
@@ -1,10 +1,53 @@
+var fog = null;
+
 function setup() {
   var canvas = createCanvas(windowWidth, 500);
   canvas.parent('interactive');
+  fog = new Fog();
 }
 
 function draw() {
-  fill(255);
-  stroke(255);
-  ellipse(mouseX, mouseY, 80, 80);
+  fog.update();
+}
+
+function Fog()  {
+  this.dots = [];
+}
+
+Fog.prototype.add = function(x, y) {
+  this.dots.push(new Dot(x, y));
+  console.log(this.dots.length);
+}
+
+Fog.prototype.update = function() {
+  clear();
+  this.add(mouseX, mouseY);
+  console.log(this.dots.length);
+
+  for (var i = 0; i < this.dots.length; i++) {
+    if (this.dots[i].lifespan > 0) {
+      this.dots[i].update();
+      this.dots[i].display();
+    } else {
+      this.dots.splice(i, 1);
+    }
+  }
+}
+
+function Dot(x, y) {
+  this.position = createVector(x, y);
+  this.lifespan = 240.0;
+}
+
+Dot.prototype.update = function() {
+  this.lifespan -= 8.0;
+  //console.log(this.lifespan);
+}
+
+Dot.prototype.display = function() {
+  if (this.lifespan >  2) {
+    stroke(255, this.lifespan);
+    fill(255, this.lifespan);
+    ellipse(this.position.x, this.position.y, 150, 150);
+  }
 }
