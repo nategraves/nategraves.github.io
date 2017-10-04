c1de6513150780be5f01cf8e86e204b93cb3a53b
Nate Graves <natesgraves@gmail.com>
2017-10-04 02:15:27 -0400

    Better font color picking

--- color-preview/color-preview.js
+++ color-preview/color-preview.js
@@ -1,27 +1,36 @@
 function buildPalette() {
-    const colors = $('.input').val().split(/[ ,]+/).sort(function(a, b) {
+    const inputColors = $('.input').val().split(/[ ,]+/).sort(function(a, b) {
         return tinycolor(a).getBrightness() - tinycolor(b).getBrightness();
-    });
+    }).map((c) => tinycolor(c));
 
     const palette = $('<div class="palette tile is-parent is-12"></div>');
 
-    for (let i = 0; i < colors.length; i += 1) {
-        const color = colors[i];
-        const bgColor = tinycolor(color);
-        const bgStyle = `background-color: ${bgColor.toHexString()};`;
-        const lightClass = bgColor.isLight() ? 'light' : 'dark';
-        const colorText = `<p class="${lightClass}">${bgColor.toHexString()}</p>`;
-        const colorMarkup = $(
-        `<div class="color tile is-vertical" style="${bgStyle}">${colorText}</div>`
-        );
-
-        const colorsClone = colors.slice();
+    for (let i = 0; i < inputColors.length; i += 1) {
+        /*
+        let greys = inputColors.slice();
+        greys = greys.map((c) => {
+          return c.greyscale();
+        });
+        */
+        const color = inputColors[i];
+        const colorMarkup = $(`<div class="color tile is-vertical" style="background-color: ${color.toHexString()}"></div>`);
+        const heroColor = tinycolor.mostReadable(color, inputColors, { includeFallbackColors: false });
+        const heroText = color.toName() ? color.toName() : color.toHexString();
+        const hero = [
+          `<div class="hero">`,
+            `<h1 style="color: ${heroColor.toHexString()}">${heroText}</h1>`,
+          `</div>`
+        ].join('');
+        colorMarkup.append(hero);
+        
+        /*
+        const colorsClone = inputColors.slice();
         colorsClone.splice(i, 1)
         const randPairs = math.pickRandom(colorsClone, 2); 
         for (let j = 0; j < randPairs.length; j += 1) {
-        colorMarkup.append(`<div class="pair" style="background-color: ${tinycolor(randPairs[j]).toHexString()};"></div>`)
-
+            colorMarkup.append(`<div class="pair" style="background-color: ${tinycolor(randPairs[j]).toHexString()};"></div>`);
         }
+        */
         palette.append(colorMarkup);
     }
 
@@ -38,4 +47,8 @@ $(function() {
         e.preventDefault();
         buildPalette();
     });
+
+    $(document).on('click', '.color', function() {
+        console.log("clickly");
+    })
 })
