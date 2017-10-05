fb2f9b7f2d1bdd855d49636d0e5b4e17f09488df
Nate Graves <natesgraves@gmail.com>
2017-10-05 13:29:13 -0400

    Fix the monochromatic stuff

--- color-preview/color-preview.js
+++ color-preview/color-preview.js
@@ -1,4 +1,6 @@
 function buildPalette() {
+    $('.palettes').empty();
+
     const inputColors = $('.input').val().split(/[ ,]+/).sort(function(a, b) {
         return tinycolor(a).getBrightness() - tinycolor(b).getBrightness();
     }).map((c) => tinycolor(c));
@@ -6,32 +8,40 @@ function buildPalette() {
     const palette = $('<div class="palette tile is-parent is-12"></div>');
 
     for (let i = 0; i < inputColors.length; i += 1) {
-        /*
-        let greys = inputColors.slice();
-        greys = greys.map((c) => {
-          return c.greyscale();
-        });
-        */
         const color = inputColors[i];
+        const colorContainer = $('<div class="color-container tile is-parent is-vertical"></div>');
         const colorMarkup = $(`<div class="color tile is-vertical" style="background-color: ${color.toHexString()}"></div>`);
-        const heroColor = tinycolor.mostReadable(color, inputColors, { includeFallbackColors: false });
-        const heroText = color.toName() ? color.toName() : color.toHexString();
-        const hero = [
-          `<div class="hero">`,
-            `<h1 style="color: ${heroColor.toHexString()}">${heroText}</h1>`,
-          `</div>`
-        ].join('');
-        colorMarkup.append(hero);
-        
-        /*
-        const colorsClone = inputColors.slice();
-        colorsClone.splice(i, 1)
-        const randPairs = math.pickRandom(colorsClone, 2); 
-        for (let j = 0; j < randPairs.length; j += 1) {
-            colorMarkup.append(`<div class="pair" style="background-color: ${tinycolor(randPairs[j]).toHexString()};"></div>`);
+        let heroColor = tinycolor.mostReadable(color, inputColors);
+        const heroReadable = tinycolor.readability(color, heroColor) > 3;
+        if (!heroReadable) {
+            heroColor = tinycolor.mostReadable(color, inputColors, { includeFallbackColors: true });
         }
-        */
-        palette.append(colorMarkup);
+        const heroText = color.toName() ? color.toName() : color.toHexString();
+        colorMarkup.append(`<h1 style="color: ${heroColor.toHexString()}">${heroText}</h1>`);
+        colorContainer.append(colorMarkup);
+       
+        const colorsClone = inputColors.slice();
+        colorsClone.splice(i, 1);
+        const gradientContainer = $('<div class="gradient-container"></div>');
+        for (let j = 0; j < colorsClone.length; j += 1) {
+            const angle = 180;
+            const toColor = tinycolor(colorsClone[j]);
+            const gradient = `background-image: linear-gradient(${angle}deg, ${color.toHexString()} 0%, ${toColor.toHexString()} 100%);`;
+            gradientContainer.append(`<div class="gradient" style="${gradient}"></div>`);
+        }
+        colorContainer.append(gradientContainer);
+ 
+        const monos = tinycolor(color).monochromatic(5).slice(1, 5);
+        const monoContainer = $('<div class="mono-container"></div>');
+        for (let j = 0; j < monos.length; j += 1) {
+            monoContainer.append(`<div class="mono" style="background-color: ${tinycolor(monos[j]).toHexString()};"></div>`); 
+        }
+        colorContainer.append(monoContainer);
+
+        const controlsContainer = $('<div class="controlsContainer"><i class="fa fa-times remove-color"></i></div>');
+        colorContainer.append(controlsContainer);
+
+        palette.append(colorContainer);
     }
 
     $('.palettes').append(palette);
@@ -49,6 +59,20 @@ $(function() {
     });
 
     $(document).on('click', '.color', function() {
-        console.log("clickly");
+        $(this).toggleClass('selected');
+        const selected = $('.selected');
+        if (selected.length > 1) {
+            const colors = selected.map((el) => $(el).css('backgroundColor'));
+            $('.pairing').fadeIn(150);
+            $('.pairing').css('')
+        } else {
+            $('.pairing').fadeOut(150);
+        }
+    });
+
+    $(document).on('click', '.remove-color', function() {
+        $(this).closest('.color-container').remove();
     })
+
+    $('#colors-input').focus();
 })
