d0cddf9bb6a3e3848176f5e47c326fe3513bb24e
Nate Graves <natesgraves@gmail.com>
2017-11-10 01:27:38 -0500

    Finally working again

--- shaperator/shaperator.js
+++ shaperator/shaperator.js
@@ -1,5 +1,5 @@
-let _draw;
-let _drawnPath;
+const maxScale = 0.667;
+let bb;
 
 function generatePath() {
   $('.lloader').fadeIn();
@@ -9,9 +9,9 @@ function generatePath() {
     url: 'https://41bc3972.ngrok.io/generate',
     type: 'GET',
     success: function(response) {
-      draw(response.paths);
       $('.lloader').hide();
-      $('.svgs-container').fadeIn();
+      $('.svgs-container').show();
+      drawPaths(response.paths);
     },
     error: function(error) {
       $('.lloader').hide();
@@ -21,52 +21,57 @@ function generatePath() {
   }).done();
 }
 
-function draw(data) {
+function drawPaths(data) {
   if (Array.isArray(data)) {
     $('#svgs').empty();
     for (let i = 0; i < data.length; i++) {
       const currentPath = data[i];
-      _draw = SVG('svgs').size(500, 500);
-      _draw.rect(500, 500).fill('#ffffff').move(0, 0);
+      const _draw = SVG('svgs').size(400, 400);
+      _draw.rect(400, 400).fill('#ffffff').move(0, 0);
 
-      _drawnPath = _draw.path(currentPath);
-      _drawnPath.fill('#212025');
+      const _drawnPath = _draw.path(currentPath);
+      bb = _drawnPath.bbox();
 
-      const maxScale = 0.667;
-      const widthScale = maxScale / (_drawnPath.bbox().w / _draw.width());
-      const heightScale = maxScale / (_drawnPath.bbox().h / _draw.height());
+      const widthScale = maxScale / (bb.w / _draw.width());
+      const heightScale = maxScale / (bb.h / _draw.height());
       _drawnPath.scale(widthScale, heightScale);
+      bb = _drawnPath.bbox();
 
-      const xMove = _drawnPath.transform().x + ((_draw.width() - _drawnPath.bbox().w) / 2) - _drawnPath.bbox().x;
-      const yMove =  _drawnPath.transform().y + ((_draw.height() - _drawnPath.bbox().h) / 2) - _drawnPath.bbox().y;
+      const xMove = _drawnPath.transform().x + ((_draw.width() - bb.w) / 2) - bb.x;
+      const yMove =  _drawnPath.transform().y + ((_draw.height() - bb.h) / 2) - bb.y;
       _drawnPath.translate(xMove, yMove);
     }
   }
   else {
-    _draw = SVG('svgs').size(500, 500);
-    _draw.rect(500, 500).fill('#ffffff').move(0, 0);
+    _draw = SVG('svgs').size(400, 400);
+    _draw.rect(400, 400).fill('#ffffff').move(0, 0);
 
     _drawnPath = _draw.path(data);
-    _drawnPath.fill('#212025');
+    _drawnPath.fill('#ffffff');
+    _drawnPath.stroke('#000000');
+    bb = _drawnPath.bbox();
 
-    const maxScale = 0.667;
-    const widthScale = maxScale / (_drawnPath.bbox().w / _draw.width());
-    const heightScale = maxScale / (_drawnPath.bbox().h / _draw.height());
+    const widthScale = maxScale / (bb.w / _draw.width());
+    const heightScale = maxScale / (bb.h / _draw.height());
     _drawnPath.scale(widthScale, heightScale);
+    bb = _drawnPath.bbox();
 
-    const xMove = _drawnPath.transform().x + ((_draw.width() - _drawnPath.bbox().w) / 2) - _drawnPath.bbox().x;
-    const yMove =  _drawnPath.transform().y + ((_draw.height() - _drawnPath.bbox().h) / 2) - _drawnPath.bbox().y;
+    const xMove = _drawnPath.transform().x + ((_draw.width() - bb.w) / 2) - bb.x;
+    const yMove =  _drawnPath.transform().y + ((_draw.height() - bb.h) / 2) - bb.y;
     _drawnPath.translate(xMove, yMove);
   }
 }
 
 $(function() {
-  $('.lloader').hide();
+  $('.svgs-container').hide();
+  $('.lloader').show();
   $.ajax({
     url: 'https://41bc3972.ngrok.io',
     type: 'GET'
   }).done(function(response) {
-    draw(response.paths);
+    $('.lloader').hide();
+    $('.svgs-container').show();
+    drawPaths(response.paths);
   });
 
   $('.generate').click(function() {
@@ -77,19 +82,11 @@ $(function() {
   $(document).on('mousemove', 'svg', function(e) {
     var el = $(e.srcElement || e.target);
     var offset = el.offset();
-    /*
-    var h = (e.pageX - offset.left) / Math.round(el.width() * 0.99);
-    var s = (e.pageY - offset.top) / el.height()
-    var l = h;
-    console.log(`(${h}, ${s}, ${l})`);
-    var color = tinycolor({ h, s, l });
-    */
     var width = ($(this).width()) / 360;
     var height = ($(this).height()) / 100;
     var h = parseInt((e.pageX - offset.left) / width, 10);
     var s = parseInt((e.pageY - offset.top) / height, 10);
     var v = ((h / 3.6) + s) / 2;
-    console.log(`${h}, ${s}, ${v}`);
     var color = tinycolor({ h, s, v });
 
     var path = this.childNodes.forEach((node) => {
