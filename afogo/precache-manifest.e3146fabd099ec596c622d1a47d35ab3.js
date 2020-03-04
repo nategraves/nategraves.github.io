fcf79c3b919eba21789d11f219e97f9638ec8f09
Nate Graves <natesgraves@gmail.com>
2020-03-04 11:14:47 -0800

    new attempt at fogo

---
+++ afogo/precache-manifest.e3146fabd099ec596c622d1a47d35ab3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab30f397414c9186fdd197c08ae63c55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b65530b821b09445fea2",
+    "url": "/static/css/main.55920282.chunk.css"
+  },
+  {
+    "revision": "80286c726b161f871620",
+    "url": "/static/js/2.c0448708.chunk.js"
+  },
+  {
+    "revision": "1e9703e695a54632851c7d9e6a5a6131",
+    "url": "/static/js/2.c0448708.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b65530b821b09445fea2",
+    "url": "/static/js/main.1b98ca42.chunk.js"
+  },
+  {
+    "revision": "a7a81d0d791dc8f27ce6",
+    "url": "/static/js/runtime-main.0d7d74b9.js"
+  }
+]);
