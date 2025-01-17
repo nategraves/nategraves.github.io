89222354ef4390d7ea0f9d4ea4c38ec861813254
Nate Graves <natesgraves@gmail.com>
2025-01-16 17:01:28 -0800

    More stuffs

--- minedreader/scripts.js
+++ minedreader/scripts.js
@@ -1,5 +1,40 @@
 let canvas;
 let ctx;
+const state = {
+  "player": {
+    "position": [0, 0],
+    "strength": 100,
+    "movement": [0, 0]
+  },
+  "moles": [
+    {
+      "position": [10, 5],
+      "strength": 20,
+      "movement": [0, 0]
+    },
+    {
+      "position": [15, 8],
+      "strength": 20,
+      "movement": [0, 0]
+    }
+  ],
+  "weasels": [
+    {
+      "position": [20, 12],
+      "strength": 20,
+      "movement": [0, 0]
+    },
+    {
+      "position": [25, 15],
+      "strength": 20,
+      "movement": [0, 0]
+    }
+  ]
+}
+
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 function main() {
   // Initialize any variables or state here
@@ -14,18 +49,74 @@ function main() {
   requestAnimationFrame(animate);
 }
 
+function updatePositions() {
+  const { width, height } = canvas;
+  const { player, moles, weasels } = state;
+  // player.position[0] += 1;
+  // player.position[1] += 1;
+  moles.forEach(mole => {
+    // 30% chance of updating the movement vector by a small amount 
+    if (Math.random() > 0.1) {
+        mole.movement[0] += randomInt(-1, 1);
+        mole.movement[1] += randomInt(-1, 1);
+    }
+    // 30% chance of moving the mole by the movement vector
+    if (Math.random() > 0.1) {
+      mole.position[0] += mole.movement[0];
+      mole.position[1] += mole.movement[1];
+    }
+    // Handle if the mole is out of bounds
+    if (mole.position[0] < 0 || mole.position[0] > width) {
+      mole.movement[0] *= -1;
+    }
+    if (mole.position[1] < 0 || mole.position[1] > height) {
+      mole.movement[1] *= -1;
+    }
+  });
+  weasels.forEach(weasel => {
+    if (Math.random() > 0.1) {
+        weasel.movement[0] += randomInt(-1, 1);
+        weasel.movement[1] += randomInt(-1, 1);
+    }
+    
+    if (Math.random() > 0.1) {
+      weasel.position[0] += 1;
+      weasel.position[1] += 1;
+    }
+    if (weasel.position[0] < 0 || weasel.position[0] > width) {
+      weasel.movement[0] *= -1;
+    }
+    if (weasel.position[1] < 0 || weasel.position[1] > height) {
+      weasel.movement[1] *= -1;
+    }
+  });
+}
+
 // Put animation logic here
 function animate(timestamp) {
   const { width, height } = canvas;
   ctx.clearRect(0, 0, width, height);
   ctx.fillStyle = 'black';
-  ctx.drawRect(0, 0, width, height);
+  ctx.fillRect(0, 0, width, height);
   const { player, moles, weasels } = state;
   const playerSize = [10,20];
   ctx.fillStyle = 'white';
-  ctx.drawRect(player.position[0], player.position[1], playerSize[0], playerSize[1]);
-  console.log(player.position);
-  
+  ctx.fillRect(player.position[0], player.position[1], playerSize[0], playerSize[1]);
+
+  // Draw moles
+  ctx.fillStyle = 'green';
+  moles.forEach(mole => {
+    ctx.fillRect(mole.position[0], mole.position[1], 10, 10);
+  });
+
+  // Draw weasels
+  ctx.fillStyle = 'red';
+  weasels.forEach(weasel => {
+    ctx.fillRect(weasel.position[0], weasel.position[1], 10, 10);
+  });
+
+  updatePositions();
+
   // Request the next frame
   requestAnimationFrame(animate);
 }
